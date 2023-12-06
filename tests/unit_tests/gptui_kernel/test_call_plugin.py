6d6fc20945ccca35fd3e2ec8eee00ae594ae7805
happyapplehorse <80760121+happyapplehorse@users.noreply.github.com>
2023-12-07 00:36:43 +0800

    Ci/python publish (#19)
    * ci: install wheel and setuptools for the test environment
    
    * docs: update docs
    
    * test: patch dotenv

--- tests/unit_tests/gptui_kernel/test_call_plugin.py
+++ tests/unit_tests/gptui_kernel/test_call_plugin.py
@@ -1,25 +1,30 @@
 import os
+from unittest.mock import patch
 
 from textual.app import App
 
-import pytest
-
 from gptui.gptui_kernel.manager import Manager
 
-@pytest.mark.asyncio
+
+mocked_dotenv_values = {
+    "OPENAI_API_KEY": "fake_api_key",
+    "OPENAI_ORG_ID": "fake_org_id",
+}
+
 async def test_call_plugin():
-    app = App()
-    manager = Manager(app, dot_env_config_path=os.path.expanduser("~/.gptui/.env_gptui"))
-    _, native_plugins = manager.scan_plugins("./tests/unit_tests/gptui_kernel/plugins_test_data")
-    for plugin in native_plugins:
-        plugin_info = plugin.plugin_info
-        manager.add_plugins(plugin_info)
-    
-    add = manager.available_functions_link["add"]
-    args = {
-        "input": 1,
-        "number2": 2,
-    }
-    context = manager.gk_kernel.context_render(args, add)
-    result = await add.invoke_async(context=context)
-    assert int(float(str(result))) == 3
+    with patch('gptui.gptui_kernel.kernel.dotenv_values', return_value=mocked_dotenv_values):
+        app = App()
+        manager = Manager(app, dot_env_config_path=os.path.expanduser("~/.gptui/.env_gptui"))
+        _, native_plugins = manager.scan_plugins("./tests/unit_tests/gptui_kernel/plugins_test_data")
+        for plugin in native_plugins:
+            plugin_info = plugin.plugin_info
+            manager.add_plugins(plugin_info)
+        
+        add = manager.available_functions_link["add"]
+        args = {
+            "input": 1,
+            "number2": 2,
+        }
+        context = manager.gk_kernel.context_render(args, add)
+        result = await add.invoke_async(context=context)
+        assert int(float(str(result))) == 3
--- tests/unit_tests/gptui_kernel/test_manager.py
+++ tests/unit_tests/gptui_kernel/test_manager.py
@@ -1,15 +1,22 @@
 import os
+from unittest.mock import patch
 
 from textual.app import App
 
 from gptui.gptui_kernel.manager import Manager
 
 
+mocked_dotenv_values = {
+    "OPENAI_API_KEY": "fake_api_key",
+    "OPENAI_ORG_ID": "fake_org_id",
+}
+
 def test_scan_plugin():
-    app = App()
-    manager = Manager(app, dot_env_config_path=os.path.expanduser("~/.gptui/.env_gptui"))
-    semantic_plugins, native_plugins = manager.scan_plugins("./tests/unit_tests/gptui_kernel/plugins_test_data")
-    semantic_plugins_name_list = [plugin_meta.name for plugin_meta in semantic_plugins]
-    native_plugins_name_list = [plugin_meta.name for plugin_meta in native_plugins]
-    assert set(semantic_plugins_name_list) == {"FunSkill"}
-    assert set(native_plugins_name_list) == {"WebServe", "MathPlugin", "WriteFile"}
+    with patch('gptui.gptui_kernel.kernel.dotenv_values', return_value=mocked_dotenv_values):
+        app = App()
+        manager = Manager(app, dot_env_config_path=os.path.expanduser("~/.gptui/.env_gptui"))
+        semantic_plugins, native_plugins = manager.scan_plugins("./tests/unit_tests/gptui_kernel/plugins_test_data")
+        semantic_plugins_name_list = [plugin_meta.name for plugin_meta in semantic_plugins]
+        native_plugins_name_list = [plugin_meta.name for plugin_meta in native_plugins]
+        assert set(semantic_plugins_name_list) == {"FunSkill"}
+        assert set(native_plugins_name_list) == {"WebServe", "MathPlugin", "WriteFile"}
