475b81c7cdbd58abe4cea614cfaa2d52ead196e3
happyapplehorse <80760121+happyapplehorse@users.noreply.github.com>
2023-12-17 20:28:51 +0800

    Feature/ai care (#22)
    * feat: add ai-care
    
    * fix: avoid ai-care speaking at the same time as the user

--- src/gptui/models/openai_chat_inner_service.py
+++ src/gptui/models/openai_chat_inner_service.py
@@ -7,6 +7,7 @@ from openai import OpenAI
 from .context import OpenaiContext
 from .openai_error import OpenaiErrorHandler
 from .openai_tokens_truncate import trim_excess_tokens
+from .utils.openai_api import OpenAIClient
 from .utils.tokens_num import tokens_num_for_functions_call
 
 
@@ -16,7 +17,7 @@ gptui_logger = logging.getLogger("gptui_logger")
 def chat_service_for_inner(
         messages_list: list, 
         context: OpenaiContext,
-        openai_api_client: OpenAI,
+        openai_api_client: OpenAIClient,
         **kwargs,
     ) -> Iterable:
     
@@ -30,25 +31,28 @@ def chat_service_for_inner(
     parameters.update({"stream": True})
     parameters.update(**kwargs)
 
-    offset_tokens_num = -tokens_num_for_functions_call(parameters["tools"], model=inner_context.parameters["model"])
-    trim_messages = trim_excess_tokens(inner_context, offset=offset_tokens_num)
+    if tools_info := parameters.get("tools"):
+        offset_tokens_num = -tokens_num_for_functions_call(tools_info, model=inner_context.parameters["model"])
+    else:
+        offset_tokens_num = 0
+    trimmed_messages = trim_excess_tokens(inner_context, offset=offset_tokens_num)
     
     # Delete the tool reply messages at the beginning of the information list.
     # This is because if the information starts with a function reply message,
     # it indicates that the function call information has already been truncated.
     # The OpenAI API requires that function reply messages must be responses to function calls.
     # Therefore, if the function reply messages are not removed, it will result in an OpenAI API error.
-    while trim_messages and trim_messages[0].get("role") == "tool":
-        trim_messages.pop(0)
+    while trimmed_messages and trimmed_messages[0].get("role") == "tool":
+        trimmed_messages.pop(0)
 
     try:
         response = openai_api_client.with_options(timeout=20.0).chat.completions.create(
-            messages=trim_messages,
+            messages=trimmed_messages,
             **parameters,
             )
     except Exception as e:
-        gptui_logger.debug('----trim_messages----in chat inner')
-        gptui_logger.debug(trim_messages)
+        gptui_logger.debug('----trimmed_messages----in chat inner')
+        gptui_logger.debug(trimmed_messages)
         # The OpenAI API interface is a time-consuming synchronous interface, so it should be called in a new thread, hence there is no event loop here.
         OpenaiErrorHandler().openai_error_handle(error=e, context=inner_context, event_loop=False)
         raise e
--- src/gptui/models/utils/openai_api.py
+++ src/gptui/models/utils/openai_api.py
@@ -4,13 +4,16 @@ from openai import OpenAI, AsyncOpenAI
 from .openai_settings_from_dot_env import openai_settings_from_dot_env
 
 
+OpenAIClient = OpenAI | AsyncOpenAI
+
+
 def openai_api(dot_env_path: str | None):
     assert dot_env_path, "'dot_env_path' can not be None or empty."
     openai_key, org_id = openai_settings_from_dot_env(dot_env_path)
     openai.api_key = openai_key
     return openai
 
-def openai_api_client(dot_env_path: str | None, async_client: bool = False, **kwargs):
+def openai_api_client(dot_env_path: str | None, async_client: bool = False, **kwargs) -> OpenAIClient:
     assert dot_env_path, "'dot_env_path' can not be None or empty."
     openai_key, org_id = openai_settings_from_dot_env(dot_env_path)
     if async_client is True:
