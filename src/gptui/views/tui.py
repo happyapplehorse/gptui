220553f8a31b092cfc0a160bde742a9d62ea771f
Xueao Chao <chaoxueao@gmail.com>
2024-01-02 01:11:28 +0800

    fix(tui): fix the bugs in disposable mode caused by openai v1

--- src/gptui/views/tui.py
+++ src/gptui/views/tui.py
@@ -75,7 +75,7 @@ from ..models.doc import Doc, document_loader
 from ..models.gptui_basic_services.plugins.conversation_service import ConversationService
 from ..models.jobs import GroupTalkManager
 from ..models.openai_chat import OpenaiChat
-from ..models.utils.openai_api import openai_api
+from ..models.utils.openai_api import openai_api_client
 from ..gptui_kernel.manager import Manager
 from ..utils.my_text import MyText as Text
 from ..utils.my_text import MyLines as Lines
@@ -1748,7 +1748,7 @@ class NoContextChat:
     """
     def __init__(self, app: MainApp) -> None:
         self.app = app
-        self.openai_api = openai_api(app.config["dot_env_path"])
+        self.openai_api_client = openai_api_client(app.config["dot_env_path"])
         self.count = 0
         self.no_context_chat_active = 0
         self.no_context_chat_dict = {}
@@ -1773,7 +1773,7 @@ class NoContextChat:
         ani_id = str(uuid.uuid4())
         app.post_message(AnimationRequest(ani_id=ani_id, action="start"))
         try:
-            response = self.openai_api.ChatCompletion.create(
+            response = self.openai_api_client.with_options(timeout=20.0).chat.completions.create(
                 model = app.config["default_openai_parameters"]["model"] or "gpt-4",
                 messages = [message],
                 stream = app.config["default_openai_parameters"]["stream"],
@@ -1798,7 +1798,7 @@ class NoContextChat:
                 elif chunk.choices[0].finish_reason == "content_filter":
                     app.main_screen.query_one("#status_region").update(Text("Omitted content due to a flag from our content filters", tc("red") or "red"))
                     continue
-                chunk_message = chunk['choices'][0]['delta']['content']
+                chunk_message = chunk.choices[0].delta.content
                 collected_messages += chunk_message
                 self.chat_stream_display({"message":chunk_message, "status":"content"})
             self.chat_stream_display({"message":'', "status":"end"})
